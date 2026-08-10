586b658e17c077d9d5c7e6b151123fec84dc7402
Varnika <Varnika.32784@stu.upes.ac.in>
2026-08-10 18:13:16 +0000

    Day1: Use inputs and sum

---
+++ Day1.c
@@ -0,0 +1,23 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+/*
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include<stdio.h>
+int main() {
+  int a,b,sum;
+scanf("%d %d", &a,&b);
+ sum = a+b;
+printf("sum=%d\n",sum);
+
+return 0;
+}
