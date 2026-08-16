782a548f8d8f9132f1deb3d54b150c28eb80a42d
Varnika Sirohi <Varnika.32784@stu.upes.ac.in>
2026-08-16 11:17:50 +0530

    Update day1_Q1.c

--- day1_Q1.c
+++ day1_Q1.c
@@ -1,3 +1,18 @@
+Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
 #include<stdio.h>
 int main()
 {
