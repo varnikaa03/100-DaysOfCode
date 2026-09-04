78269e9d7e6037d3da0cf0505c28ba1f19999135
varnikaa03 <Varnika.32784@stu.upes.ac.in>
2026-09-04 18:15:00 +0000

    add day 26 q2

---
+++ day26_2.c
@@ -0,0 +1,46 @@
+// Q52: Write a program to print the following pattern:
+//
+// *
+//
+// *
+// *
+// *
+//
+// *
+// *
+// *
+// *
+// *
+//
+// *
+// *
+// *
+//
+// *
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+int i,j;
+int rows[]={1,3,5,3,1};
+
+for(i=0;i<5;i++)
+{
+for(j=0;j<rows[i];j++)
+{
+printf("*\n");
+}
+printf("\n");
+}
+
+return 0;
+}
