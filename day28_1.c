23080b305f80b4d307153edb30bf883b6debfcbb
varnikaa03 <Varnika.32784@stu.upes.ac.in>
2026-09-06 18:17:00 +0000

    add day 28 q1

---
+++ day28_1.c
@@ -0,0 +1,40 @@
+// Q55: Write a program to print all the prime numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+10
+Output 1:
+2 3 5 7
+
+Input 2:
+20
+Output 2:
+2 3 5 7 11 13 17 19
+*/
+
+#include <stdio.h>
+
+int main()
+{
+int n,i,j,count;
+scanf("%d",&n);
+
+for(i=2;i<=n;i++)
+{
+count=0;
+for(j=1;j<=i;j++)
+{
+if(i%j==0)
+{
+count++;
+}
+}
+if(count==2)
+{
+printf("%d ",i);
+}
+}
+
+return 0;
+}
