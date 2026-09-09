72be8b73d14495fc3e95a5e1626aff0b2f98ff9b
varnikaa03 <Varnika.32784@stu.upes.ac.in>
2026-09-09 18:30:00 +0000

    add day 31

---
+++ day31_2.c
@@ -0,0 +1,39 @@
+// Q62: Reverse an array without taking extra space.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+1 2 3 4
+Output 1:
+4 3 2 1
+*/
+
+#include <stdio.h>
+
+int main()
+{
+int n,i,temp;
+int a[100];
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+
+for(i=0;i<n/2;i++)
+{
+temp=a[i];
+a[i]=a[n-1-i];
+a[n-1-i]=temp;
+}
+
+for(i=0;i<n;i++)
+{
+printf("%d ",a[i]);
+}
+
+return 0;
+}
